212e5f25b42743a7e9575cb6c2c497a937d87fd9
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-11-13 23:25:23 +0530

    Added retrofit client.

--- app/src/main/java/com/curiozing/reels/data/api/ApiService.kt
+++ app/src/main/java/com/curiozing/reels/data/api/ApiService.kt
@@ -1,6 +1,21 @@
 package com.curiozing.reels.data.api
 
+import com.curiozing.reels.AppKeysAndBaseUrl
+import com.google.gson.Gson
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object ApiService {
+    private var retrofitClient: Retrofit? = null
 
-
+    fun getRetrofitClient() : Retrofit {
+        return if(retrofitClient != null){
+            retrofitClient as Retrofit
+        }else{
+            retrofitClient = Retrofit.Builder()
+                .client(OkHttpClient()).baseUrl(AppKeysAndBaseUrl.BASE_URL).addConverterFactory(GsonConverterFactory.create()).build()
+            retrofitClient as Retrofit
+        }
+    }
 }
