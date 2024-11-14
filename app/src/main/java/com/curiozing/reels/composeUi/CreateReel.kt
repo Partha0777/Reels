c9210d98fb0699996739f69dadb46f66a284cf98
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-11-14 22:14:39 +0530

    Integrated the lazyColumn to load the reels and need to desing the reels item.

--- app/src/main/java/com/curiozing/reels/composeUi/CreateReel.kt
+++ app/src/main/java/com/curiozing/reels/composeUi/CreateReel.kt
@@ -1,16 +1,41 @@
 package com.curiozing.reels.composeUi
 
-import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.lifecycle.ViewModel
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.curiozing.reels.viewModel.ReelsViewModel
-import kotlin.math.log
 
 
 @Composable
 fun CreateReel() {
-    var reelsViewModel: ReelsViewModel = viewModel()
-    Log.d("Print reels -->", reelsViewModel.reels.value.toString())
 
+    val reelsViewModel: ReelsViewModel = viewModel()
+    val reelsList =  reelsViewModel.reels.collectAsState()
+
+
+    Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+        if (reelsList.value.isNotEmpty()){
+            LazyVerticalGrid(columns = GridCells.Fixed(2),
+                content = {
+                    reelsList.value.forEach {reel ->
+                        item {
+                            Column {
+                                Text(text = reel.descriptions)
+                            }
+                        }
+                    }
+                })
+        }else{
+            CircularProgressIndicator()
+        }
+    }
 }
--- app/src/main/java/com/curiozing/reels/viewModel/ReelsViewModel.kt
+++ app/src/main/java/com/curiozing/reels/viewModel/ReelsViewModel.kt
@@ -1,8 +1,5 @@
 package com.curiozing.reels.viewModel
 
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.curiozing.reels.data.api.ApiService
