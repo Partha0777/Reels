90201e4bd5994a56bb2b521aaa799b910ce86f16
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-11-18 22:51:48 +0530

    Completed reel like and shadow.

--- app/src/main/java/com/curiozing/reels/composeUi/CreateReel.kt
+++ app/src/main/java/com/curiozing/reels/composeUi/CreateReel.kt
@@ -4,12 +4,23 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
@@ -20,6 +31,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 import com.curiozing.reels.AppKeysAndBaseUrl.IMAGE_BASE_URL
@@ -77,15 +89,24 @@ fun CreateReel() {
                                                     colors = listOf(
                                                         Color.Transparent,
                                                         Color.Black.copy(alpha = 0.1f),
-                                                        Color.Black.copy(alpha = 0.5f),
+                                                        Color.Black.copy(alpha = 0.4f),
                                                         Color.Black.copy(alpha = 0.7f),
-                                                        Color.Black.copy(alpha = 1f)
+                                                        Color.Black.copy(alpha = 9f)
                                                     ),
                                                     startY = 0f, // Start from the top
                                                     endY = reelItemHeightInPx // Dynamic height
                                                 )
-                                            )
-                                    )
+                                            ),
+                                        contentAlignment = Alignment.BottomStart
+                                    ){
+                                        Row(
+                                            verticalAlignment = Alignment.CenterVertically,
+                                            modifier = Modifier.padding(start = 12.dp, end = 12.dp, top = 6.dp, bottom = 12.dp)) {
+                                            Icon(imageVector = Icons.Default.FavoriteBorder, contentDescription = "", tint = MaterialTheme.colorScheme.primary, modifier = Modifier.size(20.dp))
+                                            Spacer(modifier = Modifier.width(4.dp))
+                                            Text(text = reel.userInteractions.likeCount.toString(), fontSize = 16.sp, color = Color.White)
+                                        }
+                                    }
                                 }
                             }
                         }
