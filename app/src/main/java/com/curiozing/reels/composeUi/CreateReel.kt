98459f577da9a4b70ca250f5205b57d48acbbe6e
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-11-21 22:48:25 +0530

    Added create reel and post CTA.

--- app/src/main/java/com/curiozing/reels/composeUi/CreateReel.kt
+++ app/src/main/java/com/curiozing/reels/composeUi/CreateReel.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
