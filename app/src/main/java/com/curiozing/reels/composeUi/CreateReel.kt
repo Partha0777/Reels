ee1b9c49da60d4fe9ac51ac7a638eff68c33baf3
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-11-22 22:47:46 +0530

    Added clickable with ripple effect.

--- app/src/main/java/com/curiozing/reels/composeUi/CreateReel.kt
+++ app/src/main/java/com/curiozing/reels/composeUi/CreateReel.kt
@@ -2,6 +2,7 @@ package com.curiozing.reels.composeUi
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -16,13 +17,17 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.ripple
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -41,10 +46,12 @@ fun CreateReel() {
                 modifier = Modifier
                     .clip(shape = RoundedCornerShape(20))
                     .background(MaterialTheme.colorScheme.primary)
+                    .clickable(onClick = {},
+                        indication = ripple(bounded = true, color = Color.Gray),
+                        interactionSource = remember { MutableInteractionSource() }
+                        )
                     .padding(start = 20.dp, end = 20.dp, top = 80.dp, bottom = 80.dp)
-                    .clickable {
 
-                    }
             ) {
                 Column(
                     horizontalAlignment = Alignment.CenterHorizontally
