871295e15825e5e6535c2d15dc2ad0d529e44531
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-12-02 22:39:02 +0530

    Added firestore libs for video file uploading functionality

--- app/src/main/java/com/curiozing/reels/model/reels/Reels.kt
+++ app/src/main/java/com/curiozing/reels/model/reels/Reels.kt
@@ -1,11 +1,11 @@
 package com.curiozing.reels.model.reels
 
 data class Reels(
-    val id:Long,
-    val descriptions:String,
-    val thumbnail:String,
-    val videoUrl:String,
-    val createdAt:Long,
+    val id: Long,
+    val descriptions: String,
+    val thumbnail: String,
+    val videoUrl: String,
+    val createdAt: Long,
     val userInteractions: UserInteractions
 )
 
