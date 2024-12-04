1b258eb08d61cfc1fb4a0165cb068158f4ecce30
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-12-04 22:01:01 +0530

    Removed firebase sdk from project since it's not supporting firebase storage options for free plan.

--- app/src/main/java/com/curiozing/reels/utils/VideoUploadManger.kt
+++ app/src/main/java/com/curiozing/reels/utils/VideoUploadManger.kt
@@ -1,13 +1,12 @@
 package com.curiozing.reels.utils
 
-import com.google.firebase.storage.FirebaseStorage
-import com.google.firebase.storage.StorageReference
+
+
+
 
 
 class VideoUploadManger {
 
-    val storage = FirebaseStorage.getInstance()
-    val storageRef: StorageReference = storage.reference
 
     fun uploadVideo(){
 
