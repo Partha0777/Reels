f099308bd0ec983cf67d70fdc61ae87266651ddf
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-12-05 22:46:31 +0530

    Added cloudinary lib support.

--- app/src/main/java/com/curiozing/reels/utils/VideoUploadManger.kt
+++ app/src/main/java/com/curiozing/reels/utils/VideoUploadManger.kt
@@ -1,8 +1,34 @@
 package com.curiozing.reels.utils
 
+import com.cloudinary.android.MediaManager
+import com.cloudinary.android.callback.ErrorInfo
+import com.cloudinary.android.callback.UploadCallback
+
 class CloudinaryUploader : VideoUploadManger {
     override fun uploadVideo() {
+        MediaManager.get().upload("").callback(object :UploadCallback{
+            override fun onStart(requestId: String?) {
+                TODO("Not yet implemented")
+            }
 
+            override fun onProgress(requestId: String?, bytes: Long, totalBytes: Long) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onSuccess(requestId: String?, resultData: MutableMap<Any?, Any?>?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onError(requestId: String?, error: ErrorInfo?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onReschedule(requestId: String?, error: ErrorInfo?) {
+                TODO("Not yet implemented")
+            }
+
+
+        })
     }
 
     override fun updateVideoUrlToDatabase() {
