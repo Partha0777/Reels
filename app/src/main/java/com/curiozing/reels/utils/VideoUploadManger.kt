dcd7082d5954c9d1b25f8bab4b1bf791410680a0
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-12-10 22:23:24 +0530

    Added video upload manager.

--- app/src/main/java/com/curiozing/reels/utils/VideoUploadManger.kt
+++ app/src/main/java/com/curiozing/reels/utils/VideoUploadManger.kt
@@ -1,5 +1,6 @@
 package com.curiozing.reels.utils
 
+import android.util.Log
 import com.cloudinary.android.MediaManager
 import com.cloudinary.android.callback.ErrorInfo
 import com.cloudinary.android.callback.UploadCallback
@@ -8,22 +9,30 @@ class CloudinaryUploader : VideoUploadManger {
     override fun uploadVideo(path:String,onVideoUploading : () ->Unit, onVideoUploadSuccess : () ->Unit,onVideoUploadFailure : () ->Unit) {
 
         MediaManager.get().upload(path).callback(object : UploadCallback {
-            override fun onStart(requestId: String?) {}
+            override fun onStart(requestId: String?) {
+                Log.d("Started Uploading", "")
+            }
 
             override fun onProgress(requestId: String?, bytes: Long, totalBytes: Long) {
+                Log.d("onProgress Uploading", "$bytes")
+
                 onVideoUploading()
             }
 
             override fun onSuccess(requestId: String?, resultData: MutableMap<Any?, Any?>?) {
+                Log.d("onProgress Success", "")
+
                 onVideoUploadSuccess()
             }
 
             override fun onError(requestId: String?, error: ErrorInfo?) {
+                Log.d("onProgress Error", "$error")
+
                 onVideoUploadFailure()
             }
 
             override fun onReschedule(requestId: String?, error: ErrorInfo?) {}
-        })
+        }).dispatch()
     }
 
     override fun updateVideoUrlToDatabase() {}
