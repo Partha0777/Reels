b653c8b566c88afbf88e9055c696b50141315eac
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-12-14 19:09:41 +0530

    Added upload progress conversion to percentage.

--- app/src/main/java/com/curiozing/reels/viewModel/CreateReelViewModel.kt
+++ app/src/main/java/com/curiozing/reels/viewModel/CreateReelViewModel.kt
@@ -1,15 +1,21 @@
 package com.curiozing.reels.viewModel
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.curiozing.reels.utils.CloudinaryUploader
 import com.curiozing.reels.utils.VideoUploadManger
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 class CreateReelViewModel : ViewModel() {
 
     private val videoUploadManger: VideoUploadManger = CloudinaryUploader()
 
+    val progress:MutableStateFlow<Int> = MutableStateFlow(0)
+
     fun startRecording(path: String) {
-        videoUploadManger.uploadVideo(path, {
+        videoUploadManger.uploadVideo(path, {progressValue ->
+            progress.value = progressValue
         }, {
         }, {
         })
