647f575c643e9816a30675543ea3549559752daa
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-12-19 22:42:29 +0530

    Added video post page for post the video

--- app/src/main/java/com/curiozing/reels/composeUi/VideoPostPage.kt
+++
@@ -1,4 +0,0 @@
-package com.curiozing.reels.composeUi
-
-class VideoPostPage {
-}
---
+++ app/src/main/java/com/curiozing/reels/composeUi/VideoPostView.kt
@@ -0,0 +1,8 @@
+package com.curiozing.reels.composeUi
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun VideoPostView(){
+
+}
