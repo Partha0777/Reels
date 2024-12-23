68633399e569d1664f9d42a81c738861e0c8b28c
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-12-23 19:50:52 +0530

    Added Exo player lib for view the recorded video.

--- app/src/main/java/com/curiozing/reels/composeUi/VideoPostView.kt
+++ app/src/main/java/com/curiozing/reels/composeUi/VideoPostView.kt
@@ -1,8 +1,12 @@
 package com.curiozing.reels.composeUi
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 
 @Composable
 fun VideoPostView(){
 
+    Box {
+
+    }
 }
