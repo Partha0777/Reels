1e01e402916d065949b954827c445defc094488b
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-12-25 12:14:51 +0530

    Removed unwanted buttons from video player.

--- app/src/main/java/com/curiozing/reels/composeUi/VideoPostView.kt
+++ app/src/main/java/com/curiozing/reels/composeUi/VideoPostView.kt
@@ -1,21 +1,16 @@
 package com.curiozing.reels.composeUi
 
-import android.content.Context
 import android.net.Uri
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
-import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.PlayerView
-import com.curiozing.reels.R
 
 
 @Composable
